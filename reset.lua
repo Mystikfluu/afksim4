197b9578aee322f2f660d654f39dae80c8da3009
Mystikfluu <50167590+Mystikfluu@users.noreply.github.com>
2020-12-27 17:12:34 +0100

    Create reset.lua

---
+++ reset.lua
@@ -0,0 +1,33 @@
+function getvisible()
+    return game.Players.LocalPlayer.PlayerGui.GameGUI.Multiplier.Visible
+end
+function enablemultgui()
+local x,y = 647.44,85
+repeat wait() until iswindowactive()
+mousemoveabs(x,y)
+mousemoveabs(x,y)
+mousemoveabs(x,y)
+wait()
+mousemoveabs(x,y)
+mousemoveabs(x,y)
+mouse1click()
+mousemoveabs(x,y)
+mousemoveabs(x,y)
+wait(0.1)
+return getvisible()
+end
+function reset()
+    if(not(getvisible())) then
+        enablemultgui()
+    end
+    repeat wait() until iswindowactive()
+    local x,y = 830,650
+    mousemoveabs(x,y)
+    wait()
+    mousemoveabs(x,y)
+    mouse1click()
+    mousemoveabs(x,y)
+end
+while _G.reset do
+reset()
+end
